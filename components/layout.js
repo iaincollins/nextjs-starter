3f45d12d5d0b800e42981b9210405892e0cd054c
Iain Collins <me@iaincollins.com>
2017-01-18 23:39:39 +0000

    Removing unused dependacy in layout
    Also updating layout to pass null session object if one is not defined for any reason (i.e. hasn’t been passed to it) which doesn’t happen in the example but is good practice and prevents an error occuring if someone were to make a new page that uses the Layout component but not extend it from the Page component (or set this.props.session themselves).

--- components/layout.js
+++ components/layout.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import Header from './header'
 import Footer from './footer'
-import Session from './session'
 
 export default class extends React.Component {
 
   render() {
     return(
       <div>
-        <Header session={this.props.session}/>
+        <Header session={this.props.session || null}/>
           { this.props.children }
         <Footer />
       </div>
