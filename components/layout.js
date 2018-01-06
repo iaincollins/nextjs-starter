ba3df1793a5a48c095d2c678fd97173aea270790
Iain Collins <me@iaincollins.com>
2018-01-06 12:16:40 +0000

    Dropdown and nav menu toggle improvements
    Partly addresses issues in #57
    
    * Dropdown menus in the nav menu on mobile now display correctly.
    * The nav menu no longer requires JavaScript to be enabled to work and uses a pure CSS solution. I may prototype to extend this to other components and see if it’s a pull request the Reactstrap folks would be interested in.

--- components/layout.js
+++ components/layout.js
@@ -28,14 +28,7 @@ export default class extends React.Component {
       isOpen: false,
       modal: false
     }
-    this.toggleNavbar = this.toggleNavbar.bind(this)
     this.toggleModal = this.toggleModal.bind(this)
-  }
-
-  toggleNavbar() {
-    this.setState({
-      isOpen: !this.state.isOpen
-    })
   }
 
   toggleModal(e) {
@@ -65,34 +58,66 @@ export default class extends React.Component {
               <span className="icon ion-md-home mr-1"></span> {Package.name}
             </NavbarBrand>
           </Link>
-          <NavbarToggler onClick={this.toggleNavbar}/>
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav navbar>
-              <UncontrolledDropdown nav inNavbar>
-                <DropdownToggle nav caret>
-                  Examples
-                </DropdownToggle>
-                <DropdownMenu>
-                  <Link prefetch href="/examples/layout">
-                    <a href="/examples/layout" className="dropdown-item">Layout</a>
-                  </Link>
-                  <Link prefetch href="/examples/styling">
-                    <a href="/examples" className="dropdown-item">Styling</a>
-                  </Link>
-                  <Link prefetch href="/examples/async">
-                    <a href="/examples/async" className="dropdown-item">Async Data</a>
-                  </Link>
-                  <Link prefetch href="/examples/routing">
-                    <a href="/examples/routing" className="dropdown-item">Routing</a>
-                  </Link>
-                  <Link prefetch href="/examples/authentication">
-                    <a href="/examples/authentication" className="dropdown-item">Authentication</a>
-                  </Link>
-                </DropdownMenu>
-              </UncontrolledDropdown>
-            </Nav>
-            <UserMenu session={this.props.session} toggleModal={this.toggleModal}/>
-          </Collapse>
+          <style jsx global>{`
+            /* This is a pure-CSS solution for navbar menu toggles */
+            .menu-toggle-label {
+              font-size: 1.5em;
+              margin: 0 5px 0 0;
+            }
+            #navbar-menu,
+            #navbar-menu-toggle {
+              display: none;
+              clear: both;
+              width: 100%;
+            }
+            #navbar-menu-toggle:checked + #navbar-menu {
+              display: block;
+            }
+            #navbar-menu .dropdown {
+              display: block;
+            }
+            @media (min-width: 768px) {
+              .menu-toggle-label {
+                display: none;
+              }
+              #navbar-menu {
+                display: block;
+              } 
+            }
+          `}</style>
+          <label className="menu-toggle-label" for="navbar-menu-toggle">
+            <span className="con ion-md-menu"></span>
+          </label>
+          <input type="checkbox" id="navbar-menu-toggle"/>
+          <div id="navbar-menu">
+            <Collapse isOpen={true} navbar>
+              <Nav navbar>
+                <UncontrolledDropdown nav inNavbar>
+                  <DropdownToggle nav caret>
+                    Examples
+                  </DropdownToggle>
+                  <DropdownMenu>
+                    <Link prefetch href="/examples/layout">
+                      <a href="/examples/layout" className="dropdown-item">Layout</a>
+                    </Link>
+                    <Link prefetch href="/examples/styling">
+                      <a href="/examples" className="dropdown-item">Styling</a>
+                    </Link>
+                    <Link prefetch href="/examples/async">
+                      <a href="/examples/async" className="dropdown-item">Async Data</a>
+                    </Link>
+                    <Link prefetch href="/examples/routing">
+                      <a href="/examples/routing" className="dropdown-item">Routing</a>
+                    </Link>
+                    <Link prefetch href="/examples/authentication">
+                      <a href="/examples/authentication" className="dropdown-item">Authentication</a>
+                    </Link>
+                  </DropdownMenu>
+                </UncontrolledDropdown>
+              </Nav>
+              <UserMenu session={this.props.session} toggleModal={this.toggleModal}/>
+            </Collapse>
+          </div>
         </Navbar>
         <MainBody navmenu={this.props.navmenu} fluid={this.props.fluid} container={this.props.container}>
           {this.props.children}
